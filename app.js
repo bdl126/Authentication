a4647db94d05ccd725d806187ddd01cb4ae52ae3
Bruno <bdelafontaine92@gmail.com>
2022-03-07 11:53:56 -0500

    Use bscrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const app = express();
 
@@ -19,10 +20,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 
 const User = new mongoose.model("User", userSchema)
 
@@ -39,31 +36,41 @@ app.get("/register", (req,res) => {
 })
 
 app.post("/register",(req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save(function(err) {
-        if(err){
-            console.log(err);
-        } else {
-            res.render("secrets")
-        }
-    })
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save(function(err) {
+            if(err){
+                console.log(err);
+            } else {
+                res.render("secrets")
+            }
+        })
+    });
+
+
 })
 
 app.post("/login", (req,res)=> {
     const username = req.body.username
     const password = req.body.password
 
+
     User.findOne({email: username}, (err,foundUser)=>{
         if(err){
             console.log(err);
         } else {
             if(foundUser){
-                if (password === foundUser.password){
-                    res.render("secrets")
-                }
+                bcrypt.compare(password,foundUser.password, (err,same) => {
+                    if(!err){
+                        if (same === true){
+                            res.render("secrets")
+                        }
+                    }
+                })
             }
         }
     })
